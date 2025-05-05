0399cfe453f1e48e7441582b1d8d62c718f17f1d
Nithh <114797570+Nithhzzy@users.noreply.github.com>
2025-05-05 19:31:40 +0000

    up

--- NTTC-Course/backend/models/Course.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const CourseSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  description: String,
-  lessons: [String],
-}, { timestamps: true });
-
-module.exports = mongoose.model('Course', CourseSchema);
--- NTTC-Course/nuxt.config.ts
+++
@@ -1,3 +0,0 @@
-export default defineNuxtConfig({
-  css: ['@/assets/css/tailwind.css']
-});
